7de7fb9988112e2c2f531ccd49ce5db455f5bdfb
Seung Hyun Kim <skim449@illinois.edu>
2022-02-16 06:50:59 -0600

    add python modules

---
+++ elastica_rhino/callback_functions.py
@@ -0,0 +1,16 @@
+__doc__ = """ Modules containing callback classes for Rhino visualization """
+
+import warnings
+import os
+import sys
+import numpy as np
+
+from elastica.callback_functions import CallBackBaseClass
+
+
+class CallbackCollection:
+    pass
+
+
+class RodCallback(CallBackBaseClass):
+    pass
